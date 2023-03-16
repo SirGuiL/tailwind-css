5a2f143a1e9142063b49ec44591f769ad6bb7efe
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-03-15 22:27:18 -0300

    purge

---
+++ Aula12/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  // darkMode: 'media',
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'cidade': 'url(../img/bg_cidade.jpg)',
+        'tailwind': 'url(../img/tailwind.png)'
+      })
+    },
+  },
+  plugins: [],
+}
