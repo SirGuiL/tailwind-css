96dd3dec3f41bdbb9f31360a58718d3fa8d1fd16
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-03-15 21:49:55 -0300

    dark mode

--- Aula10/tailwind.config.js
+++ Aula10/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  darkMode: 'media',
+  // darkMode: 'media',
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: theme => ({
